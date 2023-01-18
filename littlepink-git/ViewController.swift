2853cfa27677b4f25e74a23d063b5c53456905dd
SHI YUHANG <17625068893@163.com>
2023-01-18 16:44:36 +0900

    Initial Commit

---
+++ littlepink-git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  littlepink-git
+//
+//  Created by shiyuhang on 2023/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
